31417a485753358316bf8fd9ce7117072e9648c7
pedroghiotti <pedro.ghiotti.m@gmail.com>
2025-05-05 15:02:15 -0300

    feat: refatorada implementação da classe User, utilizando record.

--- strategy-example/src/notifications/SmsNotificationGateway.java
+++ strategy-example/src/notifications/SmsNotificationGateway.java
@@ -3,10 +3,8 @@ package notifications;
 import entity.User;
 
 public class SmsNotificationGateway implements NotificationGateway {
-
     @Override
     public void notify(User user, String message) {
-        System.out.println(String.format("Notificando ao usuário %s por SMS... Mensagem: %s", user.getName(), message));
+        System.out.println(String.format("Notificando ao usuário %s por SMS... Mensagem: %s", user.name(), message));
     }
-
 }
--- strategy-example/src/service/NotificationService.java
+++ strategy-example/src/service/NotificationService.java
@@ -4,7 +4,7 @@ import notifications.NotificationChannel;
 
 public class NotificationService {
     public void notify(User user, String message) {
-        user.getPreferredNotificationChannels()
+        user.preferredNotificationChannels()
             .forEach((NotificationChannel notificationChannel) -> notificationChannel.notify(user, message));
     }
 }
